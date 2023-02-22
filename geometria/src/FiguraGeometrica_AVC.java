1ea5d6bd46830c2d23cbe0bbd1432fca44a1f9f5
alexvc991 <alexvc2399@gmail.com>
2023-02-22 16:24:37 +0100

    version refactorizada

--- geometria/src/FiguraGeometrica_AVC.java
+++ geometria/src/FiguraGeometrica_AVC.java
@@ -1,9 +1,9 @@
 
-public abstract class FiguraGeometrica {
+public abstract class FiguraGeometrica_AVC {
 
 	protected String tipoFigura;
 
-	public FiguraGeometrica(String tipoFigura) {
+	public FiguraGeometrica_AVC(String tipoFigura) {
 		super();
 		this.tipoFigura = tipoFigura;
 	}
